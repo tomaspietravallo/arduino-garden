3d40b118a6e860d6e884a3e792add5d3d46d0aec
Tomás Pietravallo <pietravallo21tomas@gmail.com>
2022-09-25 18:16:58 -0300

    Update

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,30 +8,79 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import * as Functions from '@google-cloud/functions-framework';
-import * as BigQuery from '@google-cloud/bigquery';
-const PROJECT_ID = process.env.PROJECT_ID;
-const DATASET = process.env.DATASET;
-const TABLE = process.env.TABLE;
-const bigQuery = new BigQuery.BigQuery();
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var Functions = require("@google-cloud/functions-framework");
+var BigQuery = require("@google-cloud/bigquery");
+var PROJECT_ID = process.env.PROJECT_ID;
+var DATASET = process.env.DATASET;
+var TABLE = process.env.TABLE;
+var bigQuery = new BigQuery.BigQuery();
 ;
 function logData(req) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const rows = req.body.arduino_data
-            .map((data) => ({ date: data.u, soil_humidity: data.h, temperature: data.t }));
-        return yield bigQuery
-            .dataset(DATASET, { projectId: PROJECT_ID })
-            .table(TABLE)
-            .insert(rows);
+    return __awaiter(this, void 0, void 0, function () {
+        var rows;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    console.log(JSON.stringify(req.body));
+                    console.log(JSON.stringify(req.body.arduino_data));
+                    console.log(req.body.arduino_data);
+                    rows = req.body.arduino_data
+                        .map(function (data) { return ({ date: data.u, soil_humidity: data.h, temperature: data.t }); });
+                    return [4 /*yield*/, bigQuery
+                            .dataset(DATASET, { projectId: PROJECT_ID })
+                            .table(TABLE)
+                            .insert(rows)];
+                case 1: return [2 /*return*/, _a.sent()];
+            }
+        });
     });
 }
 function pushNotificationToUser() {
-    return __awaiter(this, void 0, void 0, function* () {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/];
+        });
     });
 }
-const entry = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const resolveRequest = yield logData(req);
-    console.log(resolveRequest);
-    res.status(200);
-});
+var entry = function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var resolveRequest;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, logData(req)];
+            case 1:
+                resolveRequest = _a.sent();
+                console.log(resolveRequest);
+                res.status(200);
+                return [2 /*return*/];
+        }
+    });
+}); };
 Functions.http('entry', entry);
