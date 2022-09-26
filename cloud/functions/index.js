57d4ed176932c0fa0a8aa0e6bf6805bb5003e377
Tomás Pietravallo <pietravallo21tomas@gmail.com>
2022-09-25 21:17:31 -0300

    Update

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import * as Functions from '@google-cloud/functions-framework';
 import * as BigQuery from '@google-cloud/bigquery';
 const PROJECT_ID = process.env.PROJECT_ID;
@@ -15,26 +6,22 @@ const TABLE = process.env.TABLE;
 const isJest = process.env.NODE_ENV === 'test';
 const bigQuery = new BigQuery.BigQuery({ projectId: PROJECT_ID, keyFilename: (isJest ? `../../key.json` : undefined) });
 ;
-export function logData(req) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const rows = req.body.arduino_data
-            .map((data) => ({ date: new Date(data.u), soil_humidity: data.h, temperature: data.t }));
-        return yield bigQuery.dataset(DATASET)
-            .table(TABLE)
-            .insert(rows, {
-            skipInvalidRows: false,
-            ignoreUnknownValues: false,
-            createInsertId: false
-        });
+export async function logData(req) {
+    const rows = req.body.arduino_data
+        .map((data) => ({ date: new Date(data.u), soil_humidity: data.h, temperature: data.t }));
+    return await bigQuery.dataset(DATASET)
+        .table(TABLE)
+        .insert(rows, {
+        skipInvalidRows: false,
+        ignoreUnknownValues: false,
+        createInsertId: false
     });
 }
-export function pushNotificationToUser() {
-    return __awaiter(this, void 0, void 0, function* () {
-    });
+export async function pushNotificationToUser() {
 }
-export const entry = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const resolveRequest = yield logData(req);
+export const entry = async (req, res) => {
+    const resolveRequest = await logData(req);
     console.log(resolveRequest);
     res.status(200);
-});
+};
 Functions.http('entry', entry);
