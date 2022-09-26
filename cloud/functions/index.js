54be2ea178913a02b849837ee8f17ddbda2c94d6
Tomás Pietravallo <pietravallo21tomas@gmail.com>
2022-09-25 23:39:26 -0300

    Update index.js

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,24 +1,5 @@
 const Functions = require('@google-cloud/functions-framework');
 const BigQuery = require('@google-cloud/bigquery');
-const PROJECT_ID = process.env.PROJECT_ID;
-const DATASET = process.env.DATASET;
-const TABLE = process.env.TABLE;
-const bigQuery = new BigQuery.BigQuery({ projectId: PROJECT_ID });
-
-async function logData(req) {
-    const rows = req.body.arduino_data
-        .map((data) => ({ date: new Date(data.u), soil_humidity: data.h, temperature: data.t }));
-    return await bigQuery.dataset(DATASET)
-        .table(TABLE)
-        .insert(rows, {
-        skipInvalidRows: false,
-        ignoreUnknownValues: false,
-        createInsertId: false
-    });
-}
-
-async function pushNotificationToUser() {
-}
 
 const entry = async (req, res) => {
     console.log(JSON.stringify(req.body));
